256ad762d089eadd54bc0aad57760e6e9d9b67f9
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-17 18:38:29 +0300

    Update Testimonials.jsx

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,42 @@
-import './testimonials.css';
 import React from 'react';
+import './testimonials.css';
+
+import data from '../testimonials.json';
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Riview from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ id, avatar, title, name, review }) => {
+          return (
+            <SwiperSlide key={id} className="testimonials">
+              <div className="client__avatar">
+                <img src={avatar} alt={title} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
