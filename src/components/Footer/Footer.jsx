ea5bbbaf115fa02918e15b5c7033a1d7e64f16b3
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-17 18:38:11 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,56 @@
-import './footer.css';
 import React from 'react';
+import './footer.css';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#top" className="footer__logo">
+        A.L.N.
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#top">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#suggestion">Suggestion</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Alejandro Lago Navarro. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
